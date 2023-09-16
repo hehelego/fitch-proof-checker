184ff14c85a1dcd306847cc7c0e2c5c6ca2b71cf
hehelego <2364261262@qq.com>
2023-09-16 14:34:19 -0500

    data type of Fitch notation proofs

---
+++ Proof.hs
@@ -0,0 +1,19 @@
+module Proof where
+
+import Prop (Prop)
+import Rule (Rule)
+
+newtype StepRef = StepRef Int
+
+data Step
+  = WithPremise [Prop]
+  | MakeAssumption Prop.Prop Prop.Prop [Step]
+  | ApplyRule Prop.Prop Rule.Rule [StepRef]
+
+newtype Proof = Proof [Step]
+
+-- TODO: implement proof syntax check
+-- 1. A `Proof` must begin with a `WithPremise` step and end with a single conclusion proposition.
+-- 2. A `WithAssumption` block must begin with an assumption proposition and end with an colucsion proposition, which are equal to the two parameters passed to the `MakeAssumption` value constructor.
+checkProofSyntax :: Proof -> Bool
+checkProofSyntax _ = True
