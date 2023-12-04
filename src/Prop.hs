a88ea6804a08c4f9abb1a035b4b35d14b532ea32
hehelego <hahalegao@gmail.com>
2023-12-04 11:09:36 -0600

    update Prop.hs: semantics of propositional logic

--- src/Prop.hs
+++ src/Prop.hs
@@ -2,6 +2,8 @@ module Prop where
 
 type AtomId = Int
 
+type Assignment = [(AtomId, Bool)]
+
 data Prop
   = Bottom
   | Atom AtomId
@@ -18,3 +20,13 @@ instance Show Prop where
   show (p `And` q) = "(" ++ show p ++ "∧" ++ show q ++ ")"
   show (p `Or` q) = "(" ++ show p ++ "∨" ++ show q ++ ")"
   show (p `Impl` q) = "(" ++ show p ++ "→" ++ show q ++ ")"
+
+evaluate :: Prop -> Assignment -> Bool
+evaluate Bottom _ = False
+evaluate (Atom i) v = case lookup i v of
+  Just x -> x
+  Nothing -> error "Atomic proposition "
+evaluate (Not p) v = not $ evaluate p v
+evaluate (p `And` q) v = evaluate p v && evaluate q v
+evaluate (p `Or` q) v = evaluate p v || evaluate q v
+evaluate (p `Impl` q) v = not (evaluate p v) || evaluate q v
